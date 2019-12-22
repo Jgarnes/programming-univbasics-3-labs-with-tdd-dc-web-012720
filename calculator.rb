09a14ccb75c22f4b5d860178f89b21bc1c84dd41
Jeremy Garnes <j06garnes@gmail.com>
2019-12-22 19:23:51 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,14 @@
 # Add your variables here
+def first_number
+  puts "#{first_number}"
+end
+
+def second_number
+  puts "#{second_number}"
+end
+
+def addition
+  puts "#{first_number} + #{second_number}"
+end
+
+addition 
